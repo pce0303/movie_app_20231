a775c4ed5948675525feb4c654896b7c0769e54b
Choeun Park <ivychoen@gmail.com>
2023-03-11 17:28:51 +0900

    commit

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,9 +5,10 @@ function About() {
     return (
         <div className="about__container">
             <span>
-                "Freedom is the freedom to say that two plus two make four. If that is granted, all else follows."
+                저의 첫 개발로 영화 평점 웹서비스를 만들었습니다. 무료 api를 사용해 최근 20개의 영화가 평점순으로 정렬되어 있습니다.
+                제공하는 정보는 제목, 연도, 장르, 줄거리 요약입니다. 영화 카드를 클릭하면 나타나는 상세페이지에는 제목이 표시됩니다.
             </span>
-            <span>- George Orwell, 1984</span>
+            <span>- 박초은, 2023. 02. 05</span>
         </div>
     );
 }
